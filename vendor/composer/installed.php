f5f851a1f2f9f8773e4ca4595e0bce6046cbca94
kleiber <kleiber@adobe.com>
2022-01-10 13:15:32 +0100

    CSRF insatlled

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '71cfd73224c959c2245792f9e262e7c02c48810f',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '71cfd73224c959c2245792f9e262e7c02c48810f',
+            'dev_requirement' => false,
+        ),
+        'owasp/csrf-protector-php' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../owasp/csrf-protector-php',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'df629c87e4b7b5408d32f1692ea8c93fb5ac8e4e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
